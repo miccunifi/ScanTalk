7704cb598c881e35cb94471af72250ae87dc3c8c
Federico Nocentini <federico.nocentini@unifi.it>
2024-09-10 12:23:32 +0200

    Update scantalk_test.py

--- scantalk_test.py
+++ scantalk_test.py
@@ -11,7 +11,7 @@ from scantalk_dataloader import get_dataloaders
 import scipy
 import sys
 from model.scantalk import ScanTalk
-sys.path.append('./model/diffusion-net_/src')
+sys.path.append('./model/diffusion-net/src')
 
     
 def count_parameters(model):
@@ -62,29 +62,29 @@ def test(args):
 def main():
     parser = argparse.ArgumentParser(description='ScatTalk Test')
     parser.add_argument("--device", type=str, default="cuda:0")
-    parser.add_argument("--model_path", type=str, default="/equilibrium/fnocentini/ScanTalk/results/Scantalk_Masked_Velocity_Cosine_Loss_150.pth.tar")
+    parser.add_argument("--model_path", type=str, default="./results/Scantalk_Masked_Velocity_Cosine_Loss_150.pth.tar")
     parser.add_argument("--result_dir", type=str, default='./predictions')
-    parser.add_argument("--actor_file_voca", type=str, default="/equilibrium/fnocentini/Datasets/vocaset/templates.pkl", help='templates')
-    parser.add_argument("--actor_file_biwi", type=str, default="/equilibrium/fnocentini/Datasets/Biwi_6/templates", help='templates')
-    parser.add_argument("--actor_file_multiface", type=str, default="/equilibrium/fnocentini/Datasets/Multiface/templates", help='templates')
-    parser.add_argument("--template_file_voca", type=str, default="/equilibrium/fnocentini/Datasets/vocaset/flame_model/FLAME_sample.ply", help='faces to animate')
-    parser.add_argument("--template_file_biwi", type=str, default="/equilibrium/fnocentini/Datasets/Biwi_6/templates/F1.obj", help='faces to animate')
-    parser.add_argument("--template_file_multiface", type=str, default="/equilibrium/fnocentini/Datasets/Multiface/templates/20180227.ply", help='faces to animate')
+    parser.add_argument("--actor_file_voca", type=str, default="./Datasets/vocaset/templates.pkl", help='templates')
+    parser.add_argument("--actor_file_biwi", type=str, default="./Datasets/Biwi_6/templates", help='templates')
+    parser.add_argument("--actor_file_multiface", type=str, default="./Datasets/Multiface/templates", help='templates')
+    parser.add_argument("--template_file_voca", type=str, default="./Datasets/vocaset/flame_model/FLAME_sample.ply", help='faces to animate')
+    parser.add_argument("--template_file_biwi", type=str, default="./Datasets/Biwi_6/templates/F1.obj", help='faces to animate')
+    parser.add_argument("--template_file_multiface", type=str, default="./Datasets/Multiface/templates/20180227.ply", help='faces to animate')
     parser.add_argument("--train_subjects", type=str, default="FaceTalk_170728_03272_TA"
                                                               " FaceTalk_170904_00128_TA FaceTalk_170725_00137_TA FaceTalk_170915_00223_TA"
                                                               " FaceTalk_170811_03274_TA FaceTalk_170913_03279_TA"
                                                               " FaceTalk_170904_03276_TA FaceTalk_170912_03278_TA F1 F2 F3 F4 F5 F6 M1 M2 M3 M4"
-                                                              " 20171024 20180226 20180227 20180406 20180418 20180426 20180510 20180927")
+                                                              " 20171024 20180226 20180227 20180406 20180418 20180426 20180510 20180927 20190529")
     parser.add_argument("--val_subjects", type=str, default="FaceTalk_170811_03275_TA"
                                                             " FaceTalk_170908_03277_TA F7 M5 20190828 20190521")
     parser.add_argument("--test_subjects", type=str, default="FaceTalk_170809_00138_TA"
-                                                             " FaceTalk_170731_00024_TA F8 M6 20181017")
-    parser.add_argument("--wav_path_voca", type=str, default="/equilibrium/fnocentini/Datasets/vocaset/wav", help='path of the audio signals')
-    parser.add_argument("--vertices_path_voca", type=str, default="/equilibrium/fnocentini/Datasets/vocaset/vertices_npy", help='path of the ground truth')
-    parser.add_argument("--wav_path_biwi", type=str, default="/equilibrium/fnocentini/Datasets/Biwi_6/wav", help='path of the audio signals')
-    parser.add_argument("--vertices_path_biwi", type=str, default="/equilibrium/fnocentini/Datasets/Biwi_6/vertices", help='path of the ground truth')
-    parser.add_argument("--wav_path_multiface", type=str, default="/equilibrium/fnocentini/Datasets/Multiface/wav", help='path of the audio signals')
-    parser.add_argument("--vertices_path_multiface", type=str, default="/equilibrium/fnocentini/Datasets/Multiface/vertices", help='path of the ground truth')
+                                                             " FaceTalk_170731_00024_TA F8 M6 20181017 20180105")
+    parser.add_argument("--wav_path_voca", type=str, default="./Datasets/vocaset/wav", help='path of the audio signals')
+    parser.add_argument("--vertices_path_voca", type=str, default="./Datasets/vocaset/vertices_npy", help='path of the ground truth')
+    parser.add_argument("--wav_path_biwi", type=str, default="./Datasets/Biwi_6/wav", help='path of the audio signals')
+    parser.add_argument("--vertices_path_biwi", type=str, default="./Datasets/Biwi_6/vertices", help='path of the ground truth')
+    parser.add_argument("--wav_path_multiface", type=str, default="./Datasets/Multiface/wav", help='path of the audio signals')
+    parser.add_argument("--vertices_path_multiface", type=str, default="./Datasets/Multiface/vertices", help='path of the ground truth')
 
     parser.add_argument("--info", type=str, default="", help='experiment info')
     
